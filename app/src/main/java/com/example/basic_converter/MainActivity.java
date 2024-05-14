1d59f93183eaf2b8bb0e5881f8a04623dff088be
CS041 Faatih <22oo1cso41faatih@gmail.com>
2024-05-14 20:29:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/basic_converter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.basic_converter;
+
+import static com.example.basic_converter.R.color.black;
+import static com.example.basic_converter.R.color.white;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout cl;
+    private SwitchCompat s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        cl = findViewById(R.id.main);
+        s = findViewById(R.id.toggleB);
+
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    cl.setBackgroundColor(ContextCompat.getColor(MainActivity.this, black));
+                }
+                else {
+                    cl.setBackgroundColor((ContextCompat.getColor(MainActivity.this, white)));
+                }
+            }
+        });
+    }
+}
